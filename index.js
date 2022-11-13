8a258eaed5205530325f699053484328a6aa66a4
Jose-ph <68978572+Jose-ph@users.noreply.github.com>
2022-11-13 18:04:14 -0300

    adds multer

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const fs = require('fs');
+const multer = require('multer');
+
+const PORT = 8080;
 
 const app = express();
 const hbs = exphbs.create();
@@ -7,10 +11,12 @@ const hbs = exphbs.create();
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.usen(express.json());
+
 app.get('/', (req, res) => {
   res.render('home', {
     layout: 'main',
   });
 });
 
-app.listen(8080);
+app.listen(PORT);
