0741f1a288f13144b39bb6eca6420df1c44e8e0b
Jose-ph <68978572+Jose-ph@users.noreply.github.com>
2022-11-15 17:32:32 -0300

    adds multer and uploads file from form

--- routes/crud.js
+++ routes/crud.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const crudRoutes = express.Router();
 const fs = require('fs');
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
 
 const teamsPath = './data/equipos.json';
 function getTeams(path) {
@@ -12,7 +15,8 @@ function saveTeam(data, path) {
   const stringifyData = JSON.stringify(data);
   fs.writeFileSync(path, data);
 }
-
+// crudRoutes.use(express.json());
+crudRoutes.use(express.urlencoded({ extended: true }));
 crudRoutes.get('/teams', (req, res) => {
   res.render('teams', {
     layout: 'main',
@@ -23,5 +27,14 @@ crudRoutes.get('/teams/addteam', (req, res) => {
   res.render('createteamform', {
     layout: 'main',
   });
+});
+crudRoutes.post('/teams/addteam', upload.single('image'), (req, res) => {
+  const newTeamDefault = req.body;
+  newTeamDefault.area = { id: 2072, name: 'England' };
+  newTeamDefault.lastUpdated = new Date();
+  console.log(newTeamDefault);
+  // console.log('Este es el archivo subido', req.file);
+  // console.log(req.body);
+  res.status(203).send('<h1>Equipo creado con éxito</h1>');
 });
 module.exports = crudRoutes;
